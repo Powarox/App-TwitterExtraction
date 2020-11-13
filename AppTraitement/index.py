0cf18312f3981f870aa803a94f8b97e655adb093
Powarox2159 <moriniere.robin1877@gmail.com>
2020-11-13 21:10:33 +0100

    Need More opti | next stage : separation JsonBigFile -> memory error

--- AppTraitement/index.py
+++ AppTraitement/index.py
@@ -4,23 +4,20 @@ from Python.TraitementJsonFile import TraitementJsonFile
 import time
 
 
-
+start = time.time()
 # Génération des JsonFile
 traitementJsonFile = TraitementJsonFile()
-
-start = time.time()
-traitementJsonFile.openFile("AppTraitement/JsonBigFile/us_election20_tweet_pr.json", 100000)
-end = time.time()
-val = end - start
-print("Time openJs : " + str(val))
-
-start = time.time()
+traitementJsonFile.openFile("AppTraitement/JsonBigFile/us_election20_tweet_pr.json", 50000)
 traitementJsonFile.writeFile()
 end = time.time()
 val = end - start
-print("Time writeJ : " + str(val))
+print("Time OpeWri : " + str(val))
 
 
+# Memory error donc
+# diviser fichier jsonBigFile en 10 ?
+# effectuer traitement sur chaque fichier et garder les 200 best word ?
+# fusionner les resultat ?
 
 
 start = time.time()
@@ -35,7 +32,6 @@ print("Time loadJs : " + str(val))
 start = time.time()
 # Séparation Trump / Biden Tweet        -->         Traitement ? 2 filesJson sortie ?
 parsingJson.findTrumpOrBiden()
-
 trumpArray = parsingJson.getTrumpArray()
 bidenArray = parsingJson.getBidenArray()
 end = time.time()
@@ -51,6 +47,44 @@ end = time.time()
 val = end - start
 print("Time Symbol : " + str(val))
 
+
+start = time.time()
+# Count occurence mots
+trumpCountOccurence = parsingJson.countOccurenceWord(trumpWithoutSymbols)
+bidenCountOccurence = parsingJson.countOccurenceWord(bidenWithoutSymbols)
+end = time.time()
+val = end - start
+print("Time OccurW : " + str(val))
+
+
+start = time.time()
+# Trié les array par ordre décroissant
+trumpSortedArray = parsingJson.arraySorted(trumpCountOccurence)
+bidenSortedArray = parsingJson.arraySorted(bidenCountOccurence)
+end = time.time()
+val = end - start
+print("Time Sorted : " + str(val))
+
+
+start = time.time()
+# Récupération des 25 premier elements
+trumpBestElems = parsingJson.getFirstElemsArray(trumpSortedArray, 100)
+bidenBestElems = parsingJson.getFirstElemsArray(bidenSortedArray, 100)
+end = time.time()
+val = end - start
+print("Time First  : " + str(val))
+
+
+start = time.time()
+# Création file result
+parsingJson.createResultFile("Trump", trumpBestElems)
+parsingJson.createResultFile("Biden", bidenBestElems)
+end = time.time()
+val = end - start
+print("Time Result : " + str(val))
+
+
+# Test For InterfaceWeb
 
 # # -------------------- Spacy -------------------- #
 #
@@ -78,47 +112,3 @@ print("Time Symbol : " + str(val))
 # print("Time OccuSp : " + str(val))
 #
 # # -------------------- Spacy -------------------- #
-
-start = time.time()
-# Count occurence mots
-trumpCountOccurence = parsingJson.countOccurenceWord(trumpWithoutSymbols)
-bidenCountOccurence = parsingJson.countOccurenceWord(bidenWithoutSymbols)
-end = time.time()
-val = end - start
-print("Time OccurW : " + str(val))
-
-
-start = time.time()
-# Trié les array par ordre décroissant
-trumpSortedArray = parsingJson.arraySorted(trumpCountOccurence)
-bidenSortedArray = parsingJson.arraySorted(bidenCountOccurence)
-end = time.time()
-val = end - start
-print("Time Sorted : " + str(val))
-
-
-start = time.time()
-# Récupération des 25 premier elements
-trumpBestElems = parsingJson.getFirstElemsArray(trumpSortedArray, 25)
-bidenBestElems = parsingJson.getFirstElemsArray(bidenSortedArray, 25)
-end = time.time()
-val = end - start
-print("Time First  : " + str(val))
-
-
-start = time.time()
-# Création file result
-parsingJson.createResultFile("Trump", trumpBestElems)
-parsingJson.createResultFile("Biden", bidenBestElems)
-end = time.time()
-val = end - start
-print("Time Result : " + str(val))
-
-
-# Test For InterfaceWeb
-
-# [on_true] if [expression] else [on_false]
-
-
-
-# Affichage
